a33935e9e05c15c74526c729a4d0850075daed5b
ktritsh7 <ktriesh7@gmail.com>
2021-07-04 20:50:56 +0530

    Welcome to mood analyser problem

---
+++ src/main/java/com/bridgelabz/moodanalysermain/MoodAnalyser.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.moodanalysermain;
+
+public class MoodAnalyser {
+    String message;
+
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to mood analyser problem");
+    }
+}
---
+++ src/main/java/com/bridgelabz/moodanalysermain/MoodAnalyserException.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.moodanalysermain;
+
+public class MoodAnalyserException {
+}
