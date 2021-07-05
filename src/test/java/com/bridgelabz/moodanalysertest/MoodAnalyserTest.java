e0773d3ba84cbebfe891cb5688a267c063deac6d
ktritsh7 <ktriesh7@gmail.com>
2021-07-05 19:09:15 +0530

    I am in sad mood message should return SAD

--- src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
@@ -1,4 +1,17 @@
 package com.bridgelabz.moodanalysertest;
 
+import com.bridgelabz.moodanalysermain.MoodAnalyser;
+import com.bridgelabz.moodanalysermain.MoodAnalyserException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MoodAnalyserTest {
+
+    //UC1-TC1.1
+    @Test
+    void givenIAmInSadMood_shouldReturnSad() throws MoodAnalyserException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String mood = moodAnalyser.analyseMood();
+        Assertions.assertEquals(mood, "SAD");
+    }
 }
